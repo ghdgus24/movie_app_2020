cdc427f6b3b1bd730d8ceb22fa4aac250b14cf98
SYONG <ghdgus24@naver.com>
2020-08-13 18:19:24 +0900

    08/13 까지 진행한 내용 : JSX와 prop 기본 개념 및 map 함수를 통한 동적 데이터 렌더링

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const foodILike = [
+	{
+		id: 0,
+		name: "김치",
+		content: "한국인이라면",
+	},
+	{
+		id: 1,
+		name: "삼겹살",
+		content: "베스트셀러",
+	},
+	{
+		id: 2,
+		name: "라면",
+		content: "거의 주식",
+	},
+	{
+		id: 3,
+		name: "비빔밥",
+		content: "어떻게 만들어도 존맛",
+	},
+];
+
+function Food({ name, content }) {
+	return (
+		<div>
+			<h1>I love {name}</h1>
+			<h3>{content}</h3>
+		</div>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<h1>hello</h1>
+			{foodILike.map((item) => (
+				<Food name={item.name} content={item.content} key={item.id} />
+			))}
+		</div>
+	);
 }
 
 export default App;
