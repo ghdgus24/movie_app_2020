93ece307a43f6ae5dbf0cc744b63182a11dedafa
SYONG <ghdgus24@naver.com>
2020-08-14 18:27:36 +0900

    영화 api 연동 및 map 함수로 컴포넌트 렌더링

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
 	state = {
-		count: 0,
+		isLoading: true, // 마운트 되자마자 로딩해야되니까 로딩은 true
+		movies: [], // 미래에 사용할 state 미리 선언
 	};
-	add = () => {
-		this.setState({ count: this.state.count + 1 });
-	};
-	minus = () => {
-		this.setState({ count: this.state.count - 1 });
+	getMovies = async () => {
+		const {
+			data: {
+				data: { movies },
+			},
+		} = await axios.get(
+			"https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+		);
+		// getMovies 너는 기다렸다가 실행해, 뭐를 기다려? axios.get 함수를.
+		this.setState({ movies, isLoading: false });
 	};
 	componentDidMount() {
-		console.log("component rendered");
-	}
-	componentDidUpdate() {
-		console.log("I just updated");
+		this.getMovies();
 	}
 	render() {
-		console.log("render");
+		const { isLoading, movies } = this.state; // 이런식으로 설정해주면 매번 this.state를 쓰지 않아도 됨
 		return (
-			<div>
-				<h1>The number is : {this.state.count}</h1>
-				<button onClick={this.add}>add</button>
-				<button onClick={this.minus}>minus</button>
-			</div>
+			<section className="container">
+				<h1>
+					{isLoading ? (
+						<div className="loader">
+							<span className="loader__text">Loading...</span>
+						</div>
+					) : (
+						<div className="movies">
+							{movies.map((movie) => {
+								return (
+									<Movie
+										key={movie.id}
+										id={movie.id}
+										year={movie.year}
+										title={movie.title}
+										summary={movie.summary}
+										poster={movie.medium_cover_image}
+									/>
+								);
+							})}
+						</div>
+					)}
+				</h1>
+			</section>
 		);
 	}
 }
