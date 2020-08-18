c2de98787f1aea20edfc823fbc1c02b268519bbe
SYONG <ghdgus24@naver.com>
2020-08-18 18:22:53 +0900

    routing 추가 진행중.

---
+++ src/routes/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+	return (
+		<div>
+			<p>About this page: I built it because I love movies</p>
+		</div>
+	);
+}
+
+export default About;
---
+++ src/routes/Home.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./Home.css";
+import axios from "axios";
+import Movie from "../components/Movie";
+
+class Home extends React.Component {
+	state = {
+		isLoading: true, // 마운트 되자마자 로딩해야되니까 로딩은 true
+		movies: [], // 미래에 사용할 state 미리 선언
+	};
+	getMovies = async () => {
+		const {
+			data: {
+				data: { movies },
+			},
+		} = await axios.get(
+			"https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+		);
+		// getMovies 너는 기다렸다가 실행해, 뭐를 기다려? axios.get 함수를.
+		this.setState({ movies, isLoading: false });
+	};
+	componentDidMount() {
+		this.getMovies();
+	}
+	render() {
+		const { isLoading, movies } = this.state; // 이런식으로 설정해주면 매번 this.state를 쓰지 않아도 됨
+		return (
+			<section className="container">
+				<h1>
+					{isLoading ? (
+						<div className="loader">
+							<span className="loader__text">Loading...</span>
+						</div>
+					) : (
+						<div className="movies">
+							{movies.map((movie) => {
+								return (
+									<Movie
+										key={movie.id}
+										id={movie.id}
+										year={movie.year}
+										title={movie.title}
+										summary={movie.summary}
+										poster={movie.medium_cover_image}
+										genres={movie.genres}
+									/>
+								);
+							})}
+						</div>
+					)}
+				</h1>
+			</section>
+		);
+	}
+}
+
+export default Home;
