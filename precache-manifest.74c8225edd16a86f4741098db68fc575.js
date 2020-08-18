4d8c2569e8602c5f102e33e566f4c2148503cd39
SYONG <ghdgus24@naver.com>
2020-08-18 15:35:29 +0900

    Updates

---
+++ precache-manifest.74c8225edd16a86f4741098db68fc575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be40ee2ed42ee6954c55cff797875abd",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "6f50199bf02808bec228",
+    "url": "/movie_app_2020/static/css/main.75930800.chunk.css"
+  },
+  {
+    "revision": "e4c899f3ede269173b8c",
+    "url": "/movie_app_2020/static/js/2.105ade92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.105ade92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f50199bf02808bec228",
+    "url": "/movie_app_2020/static/js/main.d6b60ba0.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
